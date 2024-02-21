142870aa3152aac67b171b82e7cd38f6c9b6e7ef
JrzenonDev <jrobertoonb@gmail.com>
2024-02-20 23:08:17 -0300

    feat: create fetch api useQuery

--- src/app.tsx
+++ src/app.tsx
@@ -12,8 +12,42 @@ import {
   TableRow,
 } from "./components/ui/table";
 import { Pagination } from "./components/pagination";
+import { useQuery } from "@tanstack/react-query";
+
+export interface TagResponseProps {
+  first: number;
+  prev: number | null;
+  next: number;
+  last: number;
+  pages: number;
+  items: number;
+  data: Tag[];
+}
+
+export interface Tag {
+  title: string;
+  amountOfVideos: number;
+  id: string;
+}
 
 export function App() {
+  const { data: tagsResponse, isLoading } = useQuery<TagResponseProps>({
+    queryKey: ["get-tags"],
+    queryFn: async () => {
+      const response = await fetch(
+        "http://localhost:3333/tags?_page=1&_per_page=10"
+      );
+      const data = await response.json();
+
+      console.log(data);
+      return data;
+    },
+  });
+
+  if (isLoading) {
+    return null;
+  }
+
   return (
     <div className="py-10 space-y-8">
       <div>
@@ -52,19 +86,19 @@ export function App() {
           </TableHeader>
 
           <TableBody>
-            {Array.from({ length: 10 }).map((value, index) => {
+            {tagsResponse?.data.map((value, index) => {
               return (
                 <TableRow key={index}>
                   <TableCell></TableCell>
                   <TableCell>
                     <div className="flex flex-col gap-0.5">
-                      <span className="font-medium">React</span>
-                      <span className="text-xs text-zinc-500">
-                        9q9993213-32132131-3213213-32
-                      </span>
+                      <span className="font-medium">{value.title}</span>
+                      <span className="text-xs text-zinc-500">{value.id}</span>
                     </div>
                   </TableCell>
-                  <TableCell className="text-zinc-300">12</TableCell>
+                  <TableCell className="text-zinc-300">
+                    {value.amountOfVideos} video(s)
+                  </TableCell>
                   <TableCell className="text-right">
                     <Button size="icon">
                       <MoreHorizontal className="size-3" />
@@ -75,7 +109,13 @@ export function App() {
             })}
           </TableBody>
         </Table>
-        {/* <Pagination items={1} page={1} pages={1} /> */}
+        {tagsResponse && (
+          <Pagination
+            items={tagsResponse.items}
+            pages={tagsResponse.pages}
+            page={1}
+          />
+        )}
       </main>
     </div>
   );
