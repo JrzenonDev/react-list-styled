d51316779267d7229b8663a6053b793f5360e0e8
JrzenonDev <jrobertoonb@gmail.com>
2024-02-21 23:31:40 -0300

    feat: init dialog

--- src/app.tsx
+++ src/app.tsx
@@ -15,6 +15,7 @@ import { Pagination } from "./components/pagination";
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { useSearchParams } from "react-router-dom";
 import { useState } from "react";
+import * as Dialog from "@radix-ui/react-dialog";
 
 export interface TagResponseProps {
   first: number;
@@ -78,10 +79,32 @@ export function App() {
       <main className="max-w-6xl mx-auto space-y-5">
         <div className="flex items-center gap-3">
           <h1>Tags</h1>
-          <Button variant="primary">
-            <Plus className="size-3" />
-            Create new
-          </Button>
+
+          <Dialog.Root>
+            <Dialog.Trigger asChild>
+              <Button variant="primary">
+                <Plus className="size-3" />
+                Create new
+              </Button>
+            </Dialog.Trigger>
+
+            <Dialog.Portal>
+              <Dialog.Overlay className="fixed inset-0 bg-black/70" />
+              <Dialog.Content className="fixed p-10 right-0 top-0 bottom-0 h-screen min-w-[320px] z-10 bg-zinc-950 border-l border-zinc-900">
+                <div className="space-y-3">
+                  <Dialog.Title className="text-xl font-bold">
+                    Create tag
+                  </Dialog.Title>
+                  <Dialog.Description className="text-sm text-zinc-50">
+                    Tags can be used group videos about similar concepts.
+                  </Dialog.Description>
+                </div>
+                <Dialog.Close asChild>
+                  <Button>Close</Button>
+                </Dialog.Close>
+              </Dialog.Content>
+            </Dialog.Portal>
+          </Dialog.Root>
         </div>
 
         <div className="flex items-center justify-between">
